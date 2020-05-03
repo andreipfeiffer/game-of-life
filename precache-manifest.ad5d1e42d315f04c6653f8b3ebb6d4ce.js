909019bb20c16c061980de53b922119017d89ce5
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-05-03 16:28:30 +0300

    Updates

---
+++ precache-manifest.ad5d1e42d315f04c6653f8b3ebb6d4ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a0be2190750cee41f8f8fc843758ae",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "489c2c1bbdbbfa05c9b9",
+    "url": "/game-of-life/static/css/main.384f227e.chunk.css"
+  },
+  {
+    "revision": "ac70fc60b1f6b097734f",
+    "url": "/game-of-life/static/js/2.cf686a7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.cf686a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489c2c1bbdbbfa05c9b9",
+    "url": "/game-of-life/static/js/main.07017bdd.chunk.js"
+  },
+  {
+    "revision": "1e2ccd1259d95e53029a",
+    "url": "/game-of-life/static/js/runtime-main.56edbf87.js"
+  }
+]);
